9ba3eb905d819c1629800d1f25d90e1c2950ff84
ywLinghao <yuwenlinghao@gmail.com>
2024-01-10 18:54:46 +0800

    123

--- unpackage/dist/dev/mp-weixin/http/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const config = {
-  baseUrl: "http://api.jqrjq.cn/",
-  timeOut: 6e3,
-  env: "dev"
-};
-exports.config = config;
--- unpackage/dist/dev/mp-weixin/utils/pinia.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const common_vendor = require("../common/vendor.js");
-const pinia = common_vendor.createPinia();
-pinia.use(common_vendor.index$1);
-exports.pinia = pinia;
